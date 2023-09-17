1d3b0d36c4a73ae627aef9c56d7cd8b5c4a0c1bd
Scesar1 <scesar1@jhu.edu>
2023-09-16 21:39:59 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button className='btn btn-primary'>Button</button>
+      <button className="btn btn-secondary">Button</button>
     </main>
-  )
+  );
 }
